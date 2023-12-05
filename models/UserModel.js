8b7270d9c1571117ccb960579fd9aac91e73bc11
gabapppp <khiempham201@gmail.com>
2023-12-05 17:29:14 +0700

    modify

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    username : {type : String},
-    name : {type : String, },
-    image : {type : String},
-    about : {type : String},
-    email : {type : String, required : true,},
-    password : {type : String},
-    source : {type: String}
-},{collection : "users"})
+    username: { type: String },
+    name: { type: String, },
+    image: { type: String },
+    about: { type: String },
+    email: { type: String, required: true, },
+    password: { type: String },
+    source: { type: String }
+}, { collection: "users" })
 
 const UserModel = mongoose.model('UserSchema', userSchema)
 
