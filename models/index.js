6bdb9819f7d7d1f029c01094c4a72349e215c9c7
Loi Nguyen <20521559@gm.uit.edu.vn>
2023-12-06 16:41:49 +0700

    loi

--- models/index.js
+++ models/index.js
@@ -2,7 +2,8 @@ import UserModel from './UserModel.js';
 import RefreshTokenModel from './RefreshTokenModel.js';
 import ProductModel from './ProductModel.js';
 import UserStaffModel from './UserStaffModel.js';
+import CouponModel from './CouponModel.js';
 
 export {
-  UserModel, RefreshTokenModel, ProductModel, UserStaffModel
+  UserModel, RefreshTokenModel, ProductModel, UserStaffModel, CouponModel
 };
