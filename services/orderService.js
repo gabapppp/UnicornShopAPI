30b87ded191f0139da6eb721497e58212102dd87
gabap <khiempham201@gmail.com>
2023-12-15 02:58:14 +0700

    update

--- services/orderService.js
+++ services/orderService.js
@@ -2,8 +2,8 @@ import { OrderModel } from "../models/index.js";
 import httpStatus from 'http-status';
 import APIError from "../utils/APIError.js";
 
-const createNewOrder = async (oder) => {
-    const newOrder = await OrderModel.create(OrderModel);
+const createNewOrder = async (order) => {
+    const newOrder = await OrderModel.create({ username: order.username, fullname: order.fullname, email: order. });
     if (!newOrder)
         throw new APIError(httpStatus.BAD_REQUEST, "Oops...seems our server needed a break!")
     return newProduct;
